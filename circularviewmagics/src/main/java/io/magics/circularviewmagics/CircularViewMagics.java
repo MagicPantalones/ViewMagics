6df682472eba2252c82df6c0ff7296fad78cd3ae
MagicPants <28543678+MagicPantalones@users.noreply.github.com>
2018-01-24 21:21:20 +0100

    #1 Initial commit

---
+++ circularviewmagics/src/main/java/io/magics/circularviewmagics/CircularViewMagics.java
@@ -0,0 +1,17 @@
+package io.magics.circularviewmagics;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+/**
+ * Circular ImageView with circular padding and border.
+ * Created by Erik on 24.01.2018.
+ */
+
+public class CircularViewMagics extends android.support.v7.widget.AppCompatImageView{
+
+    public CircularViewMagics(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
